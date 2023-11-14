bf542fe0cf84352d54aa07bc2c4bf5a68c983f1d
mrmile <alvaro.milla02@gmail.com>
2023-11-14 21:10:28 +0100

    Commented unnecessary json receiving delays
    Delays are only important when sending new data since its when its updated and so, creates server trafic. We can get data from json every frame since the server will only have to update new json sent data.

--- Assets/Scripts/Gameplay/PlayerServerController.cs
+++ Assets/Scripts/Gameplay/PlayerServerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerServerController : MonoBehaviour
 {
-    public float deserializationDelay = 0.5f; // Delay for receiving data from server via json deserialization
+    //public float deserializationDelay = 0.5f; // Delay for receiving data from server via json deserialization
     public bool disableDataReceive = false; // Disable receiving data for testing or other purposes
 
-    private float lastDeserializationTime;
+    //private float lastDeserializationTime;
     private string json;
 
     PlayerStruct player;
@@ -21,12 +21,12 @@ public class PlayerServerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Time.time - lastDeserializationTime > deserializationDelay) && disableDataReceive == false)
+        if (/*(Time.time - lastDeserializationTime > deserializationDelay) &&*/ disableDataReceive == false)
         {
             JsonUtility.FromJsonOverwrite(json, player.playerTransform);
             JsonUtility.FromJsonOverwrite(json, player.isMoving);
 
-            lastDeserializationTime = Time.time;
+            //lastDeserializationTime = Time.time;
         }
 
         if (disableDataReceive == false)
