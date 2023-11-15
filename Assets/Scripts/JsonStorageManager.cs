9808b82a31d0740cb5b7ed79a47c1713a733e4d3
mrmile <alvaro.milla02@gmail.com>
2023-11-15 01:10:10 +0100

    Fixed cs script naming inconcluence

--- Assets/Scripts/JsonStorageManager.cs
+++ Assets/Scripts/JsonStorageManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class JsonManager : MonoBehaviour
+public class JsonStorageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void SaveJsonFile(string fileName, string json, string saveDirectory)
     {
-        
+        string dir = Application.persistentDataPath + saveDirectory;
+
+        if(!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+
+        File.WriteAllText(dir + fileName, json);
+
+        Debug.Log("New json file saved: " + dir + fileName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public string LoadJsonFile(string fileName, string saveDirectory)
     {
-        
+        string fullPath = Application.persistentDataPath + saveDirectory + fileName;
+        string json = "...";
+
+        if (File.Exists(fullPath))
+        {
+            json = File.ReadAllText(fullPath);
+        }
+        else
+        {
+            Debug.Log("Save Json file does not exist");
+        }
+
+        return json;
     }
 }
