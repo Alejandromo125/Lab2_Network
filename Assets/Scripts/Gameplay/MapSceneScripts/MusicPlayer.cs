e8179eca7c6da2c2e04c674b2139d6e9d54c2ad6
mrmile <73582929+mrmile@users.noreply.github.com>
2023-12-28 22:03:32 +0100

    Added music to menu scenes and new dynamic music to gameplay level map scene

---
+++ Assets/Scripts/Gameplay/MapSceneScripts/MusicPlayer.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] musicTracks; // List of music tracks.
+
+    private AudioSource audioSource;
+    private bool isPlaying = false;
+    private float loopDuration;
+    private float nextTrackTime;
+    private int lastTrackIndex = -1;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlayRandomTrack();
+    }
+
+    void Update()
+    {
+        if (isPlaying && Time.time >= nextTrackTime)
+        {
+            isPlaying = false;
+        }
+
+        if (!isPlaying)
+        {
+            PlayRandomTrack();
+        }
+    }
+
+    void PlayRandomTrack()
+    {
+        if (musicTracks.Length == 0)
+        {
+            Debug.LogWarning("No music tracks found!");
+            return;
+        }
+
+        int randomIndex = GetRandomTrackIndex();
+        while (randomIndex == lastTrackIndex) // Ensure no repetition of the same track.
+        {
+            randomIndex = GetRandomTrackIndex();
+        }
+
+        lastTrackIndex = randomIndex;
+        audioSource.clip = musicTracks[randomIndex];
+
+        loopDuration = Random.Range(90f, 180f); // Random duration between 90 to 180 seconds.
+        audioSource.Play();
+        isPlaying = true;
+        nextTrackTime = Time.time + loopDuration;
+    }
+
+    int GetRandomTrackIndex()
+    {
+        if (lastTrackIndex == musicTracks.Length - 1)
+        {
+            ShuffleTracks();
+            lastTrackIndex = -1;
+        }
+
+        int randomIndex = Random.Range(lastTrackIndex + 1, musicTracks.Length);
+        return randomIndex;
+    }
+
+    void ShuffleTracks()
+    {
+        for (int i = 0; i < musicTracks.Length; i++)
+        {
+            AudioClip temp = musicTracks[i];
+            int randomIndex = Random.Range(i, musicTracks.Length);
+            musicTracks[i] = musicTracks[randomIndex];
+            musicTracks[randomIndex] = temp;
+        }
+    }
+}
